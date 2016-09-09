db8c8d1d37942046aebd5998952b4fb4d86a9cbe
irrygan <irrygan@gmail.com>
2016-09-09 15:08:44 +0500

    12345

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import app
 from OpenSSL import SSL
+import sys
 
 if __name__ == '__main__':
-    context = ('izalith_me.crt', 'izalith_me.key')
-    app.run(debug=True, port=443, host='0.0.0.0', ssl_context=context)
+    if len(sys.argv) == 1:
+        context = ('izalith_me.crt', 'izalith_me.key')
+        app.run(debug=True, port=443, host='0.0.0.0', ssl_context=context)
+    else:
+        app.run(debug=True, port=443, host='0.0.0.0')
