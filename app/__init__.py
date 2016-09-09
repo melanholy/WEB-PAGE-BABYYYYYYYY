72f797ffef8ebe60692a11e6f819eee7f627ae58
irrygan <irrygan@gmail.com>
2016-09-09 15:19:52 +0500

    add favicon

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_sslify import SSLify
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object('config')
+app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
 
 sslify = SSLify(app)
 
