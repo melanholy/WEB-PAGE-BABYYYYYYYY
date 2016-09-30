21facad72b64c479d3b0439918a9a06324c3a14b
melanholy <irrygan@gmail.com>
2016-09-30 09:44:05 +0500

    Gather screen size and user aegent, add small improvements,

---
+++ app/static/ss.js
@@ -0,0 +1,17 @@
+function send_ss(csrf) {
+    var xhr = new XMLHttpRequest();
+
+    var ss = screen.width + 'x' + screen.height;
+    var body = 'ss=' + encodeURIComponent(ss) +
+               '&csrf_token=' + encodeURIComponent(csrf) +
+               '&path=' + encodeURIComponent(window.location.pathname) +
+               '&ua=' + encodeURIComponent(navigator.userAgent);
+
+    xhr.open('POST', '/send_ss', true);
+    xhr.setRequestHeader(
+        'Content-Type',
+        'application/x-www-form-urlencoded'
+    );
+
+    xhr.send(body);
+}
